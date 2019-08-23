a2be43560a052f4c72efe22066e65c0916fa0ad7
malekz <malek.zarzour@gmail.com>
2019-08-23 23:55:05 +0200

    refactor the server index

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+require("./services/passport");
+require("./routes/authRoutes");
+
 const app = express();
+
+require("./routes/authRoutes")(app);
+// https://sleepy-fortress-88962.herokuapp.com/
 
 // ClientID
 // ClientSecret
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    accessToken => {
-      console.log(accessToken);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
