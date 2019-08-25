adb7a0cd9a1cd4a7c1ad0f9bdb31aa34d1258d81
malekz <malek.zarzour@gmail.com>
2019-08-25 11:29:07 +0200

    added { useNewUrlParser: true } to mongoose.connect

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
-require("./models/Users");
-mongoose.connect(keys.mongoURI);
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
