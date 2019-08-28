44dd62a0aaece3b16e83e31491725210fe03f150
malekz <malek.zarzour@gmail.com>
2019-08-28 13:39:00 +0200

    file updates after creating prod deployment

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+// prod.js - production key here!!
+
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_ID_SECRET,
+  mongoURI: process.env.MONGO1_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
