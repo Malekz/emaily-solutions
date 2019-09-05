72c6ec57139ad96b27704ba5a6accdac1b690316
malekz <malek.zarzour@gmail.com>
2019-09-05 17:21:20 +0200

    refactor the code

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
-import { type } from "os";
-const fetchUser = () => {
-  return function(dispatch) {
-    axios
-      .get("/api/current_user")
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
-  };
+
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get("/api/current_user");
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+/*
+async (accessToken, refreshToken, profile, done) => {
+  const existingUser = await User.findOne({ googleId: profile.id });
+  if (existingUser) {
+    // we already have a record with thw given profile ID
+    return done(null, existingUser);
+  }
+  */
