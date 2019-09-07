7d82bdbb986d59ee52318ab511f1890197168a2a
malekz <malek.zarzour@gmail.com>
2019-09-07 15:49:39 +0200

    files refactor

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 
-class Payments extends Component {
+class Payments extends React.Component {
   render() {
-    debugger;
-
     return (
       <StripeCheckout
         amount={500}
