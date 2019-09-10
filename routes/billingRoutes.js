492c04ad07710d69b2d7920612c82cbfd175d7b2
malekz <malek.zarzour@gmail.com>
2019-09-10 22:26:48 +0200

    required body-parser

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -2,7 +2,5 @@ const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = app => {
-  app.post("/api/stripe", (req, res) => {
-    res;
-  });
+  app.post("/api/stripe", (req, res) => {});
 };
