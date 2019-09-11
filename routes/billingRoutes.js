c091d6e38248b9791ea6945b300d387d043146b0
malekz <malek.zarzour@gmail.com>
2019-09-11 22:56:56 +0200

    refactor the files

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,11 +3,18 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post("/api/stripe", async (req, res) => {
+    if (!req.user) {
+      return res.status(401).send({ error: "You must log in!" });
+    }
     const charge = await stripe.charges.create({
       amount: 500,
       currency: "usd",
       description: "$5 dollar for five credits",
       source: req.body.id
     });
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    res.send(user);
   });
 };
