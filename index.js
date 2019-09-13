09cba310506dbc39f2d2ce6e7a5d5a584f22cba1
malekz <malek.zarzour@gmail.com>
2019-09-13 09:59:30 +0200

    configured the routes in production

--- index.js
+++ index.js
@@ -27,6 +27,19 @@ require("./routes/billingRoutes")(app);
 this top require stetments will turn into functions which we then immediately call whith the express App object/
 */
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognise the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // https://sleepy-fortress-88962.herokuapp.com/
 
 const PORT = process.env.PORT || 5000;
