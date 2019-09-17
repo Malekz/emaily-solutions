9d0ec07ab58be48c5584069c0621397de87ea309
malekz <malek.zarzour@gmail.com>
2019-09-17 23:50:00 +0200

    added survey schema

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,7 +1,19 @@
 const requireLogin = require("../middlewares/requireLogin");
 
 const requireCredits = require("../middlewares/requireCredits");
+const Survey = mongoose.model("surveys");
 module.exports = app => {
   // we are saying here is " if anyone makes a post request to api/surveys, then first make sure that the user is logged in and then if they have enough credit and only then you can finally create a campaign or do whatever logic you want to do."
-  app.post("api/surveys", requireLogin, requireCredits, (req, res) => {});
+  app.post("api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(",").map(email => {
+        email;
+      })
+    });
+  });
 };
