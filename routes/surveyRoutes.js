e3b9c31b1c6e03e3511a33631dae5ca011538ff7
malekz <malek.zarzour@gmail.com>
2019-09-17 23:18:49 +0200

    required the requireCredits.js middleware

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,7 @@
 const requireLogin = require("../middlewares/requireLogin");
 
+const requireCredits = require("../middlewares/requireCredits");
 module.exports = app => {
-  app.post("api/surveys", requireLogin, (req, res) => {});
+  // we are saying here is " if anyone makes a post request to api/surveys, then first make sure that the user is logged in and then if they have enough credit and only then you can finally create a campaign or do whatever logic you want to do."
+  app.post("api/surveys", requireLogin, requireCredits, (req, res) => {});
 };
