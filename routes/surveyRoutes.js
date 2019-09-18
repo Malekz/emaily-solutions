73d4ef6fe05f83e1a13ba80eb2e9aa924e426c05
malekz <malek.zarzour@gmail.com>
2019-09-18 23:50:17 +0200

    refactor the code

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
-const Mailer = require("../services/Mailer");
 const requireCredits = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
 const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
+
 const Survey = mongoose.model("surveys");
+
 module.exports = app => {
-  // we are saying here is " if anyone makes a post request to api/surveys, then first make sure that the user is logged in and then if they have enough credit and only then you can finally create a campaign or do whatever logic you want to do."
-  app.post("api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
       title,
       subject,
       body,
-      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
+      recipients: recipients.split(",").map(email => ({ email })),
       _user: req.user.id,
       dateSent: Date.now()
     });
-    // great place to send an email!
+
+    // Great place to send an email!
     const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
   });
 };
